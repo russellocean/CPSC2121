d67976358618182e920e3fe37ba2642f36ae9d93
Russell Welch <rwelch@goldfingerinc.com>
2023-03-13 18:40:48 -0400

    Lab 6 Starter Files

---
+++ Lab 6 - Finding the Closest Pair of Points/closestPair.cpp
@@ -0,0 +1,45 @@
+/*
+ * Name:
+ * Date Submitted:
+ * Lab Section:
+ * Assignment Name:
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+struct point
+{
+    double x;
+    double y;
+};
+
+/*Implement the following function
+  Reads in a file specified by the parameter
+  Format of file: #of points on first line
+                  remaining (#of points) lines: x-value and y-value of point
+                  one point per line
+                  x-value and y-value are double-precision values and
+                  bounded by the unit square 0.0 <= x,y < 1.0
+  Should use "spatial hashing" where the #of cells scales with the #of points
+  and find the distance between the closest pair of points which will be
+  returned as a double type value
+*/
+double closestPair(string filename);
+
+int main()
+{
+    double min;
+    string filename;
+    cout << "File with list of points within unit square: ";
+    cin >> filename;
+    min = closestPair(filename);
+    cout << setprecision(16);
+    cout << "Distance between closest pair of points: " << min << endl;
+    return 0;
+}
